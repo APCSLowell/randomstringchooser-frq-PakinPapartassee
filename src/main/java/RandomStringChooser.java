bf5d5cda910051b2f55ded67d3440b7cf0075656
luigimangione#1fan <pakininninin@gmail.com>
2025-04-24 22:04:50 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -3,17 +3,16 @@ public class RandomStringChooser
 {
   private ArrayList<String> arr;
 
-  public RandomStringChooser(String [] x){
+  public RandomStringChooser(String [] wordArray){
     arr = new ArrayList<String>();
-    for(String n : x){
+    for(String n : wordArray)
       arr.add(n);
-    }
   }
 
   public String getNext(){
     if(arr.size()==0)
       return "NONE";
-    int i = (int)(Math.random*arr.size());
+    int i = (int)(Math.random()*arr.size());
     return arr.remove(i);
   }
   
